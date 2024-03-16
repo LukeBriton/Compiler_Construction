1f13d05dbae1d230a3636aca22080048d6ed5b42
Montagnard <928786745@qq.com>
2024-03-16 15:56:32 +0800

    acwj 04_Assembly

--- Lab1-1st-Gen-Compiler/src/acwj/compilerlab1.cpp
+++ Lab1-1st-Gen-Compiler/src/acwj/compilerlab1.cpp
@@ -41,6 +41,7 @@ extern_ struct token    Token;
 int     Line;
 int     Putback;
 FILE    *Infile;
+FILE    *Outfile;
 struct token    Token;
 
 // Operator precedence for each token
@@ -65,6 +66,21 @@ struct ASTnode *binexpr(int);
 int interpretAST(struct ASTnode *n);
 
 static int op_precedence(int tokentype);
+
+static int genAST(struct ASTnode *n);
+void generatecode(struct ASTnode *n);
+
+void freeall_registers();
+static int alloc_register();
+static void free_register(int reg);
+int cgload(int value);
+int cgadd(int r1, int r2);
+int cgmul(int r1, int r2);
+int cgsub(int r1, int r2);
+int cgdiv(int r1, int r2);
+void cgprintint(int r);
+void cgpreamble();
+void cgpostamble();
 
 // Get the next character from the input file.
 static int next(void) {
@@ -185,11 +201,15 @@ int main(int argc, char *argv[]) {
   init();
   
   Infile = fopen(argv[1], "r");
-  
+  Outfile = fopen("out.s", "w");
+
   //scanfile();
   scan(&Token);			// Get the first token from the input
   n = binexpr(0);		// Parse the expression in the file
   printf("%d\n", interpretAST(n));	// Calculate the final result
+  generatecode(n);
+
+  fclose(Outfile);
   exit(0);
   // return 0; // 还需要返回值吗
 }
@@ -379,4 +399,167 @@ static int op_precedence(int tokentype) {
     exit(1);
   }
   return (prec);
+}
+
+// Given an AST, generate
+// assembly code recursively
+static int genAST(struct ASTnode *n) {
+  int leftreg, rightreg;
+
+  // Get the left and right sub-tree values
+  if (n->left) leftreg = genAST(n->left);
+  if (n->right) rightreg = genAST(n->right);
+
+  switch (n->op) {
+    case A_ADD:      return (cgadd(leftreg,rightreg));
+    case A_SUBTRACT: return (cgsub(leftreg,rightreg));
+    case A_MULTIPLY: return (cgmul(leftreg,rightreg));
+    case A_DIVIDE:   return (cgdiv(leftreg,rightreg));
+    case A_INTLIT:   return (cgload(n->intvalue));
+
+    default:
+      fprintf(stderr, "Unknown AST operator %d\n", n->op);
+      exit(1);
+  }
+}
+
+void generatecode(struct ASTnode *n) {
+  int reg;
+
+  cgpreamble();
+  reg= genAST(n);
+  cgprintint(reg);      // Print the register with the result as an int
+  cgpostamble();
+}
+
+// List of available registers
+// and their names
+static int freereg[4];
+static char *reglist[4]= { "%r8", "%r9", "%r10", "%r11" };
+
+// Set all registers as available
+void freeall_registers(void)
+{
+  freereg[0]= freereg[1]= freereg[2]= freereg[3]= 1;
+}
+
+
+// Allocate a free register. Return the number of
+// the register. Die if no available registers.
+static int alloc_register(void)
+{
+  for (int i=0; i<4; i++) {
+    if (freereg[i]) {
+      freereg[i]= 0;
+      return(i);
+    }
+  }
+  fprintf(stderr, "Out of registers!\n");
+  exit(1);
+}
+
+
+// Return a register to the list of available registers.
+// Check to see if it's not already there.
+static void free_register(int reg)
+{
+  if (freereg[reg] != 0) {
+    fprintf(stderr, "Error trying to free register %d\n", reg);
+    exit(1);
+  }
+  freereg[reg]= 1;
+}
+
+
+// Print out the assembly preamble
+void cgpreamble()
+{
+  freeall_registers();
+  fputs(
+	"\t.text\n"
+	".LC0:\n"
+	"\t.string\t\"%d\\n\"\n"
+	"printint:\n"
+	"\tpushq\t%rbp\n"
+	"\tmovq\t%rsp, %rbp\n"
+	"\tsubq\t$16, %rsp\n"
+	"\tmovl\t%edi, -4(%rbp)\n"
+	"\tmovl\t-4(%rbp), %eax\n"
+	"\tmovl\t%eax, %esi\n"
+	"\tleaq	.LC0(%rip), %rdi\n"
+	"\tmovl	$0, %eax\n"
+	"\tcall	printf@PLT\n"
+	"\tnop\n"
+	"\tleave\n"
+	"\tret\n"
+	"\n"
+	"\t.globl\tmain\n"
+	"\t.type\tmain, @function\n"
+	"main:\n"
+	"\tpushq\t%rbp\n"
+	"\tmovq	%rsp, %rbp\n",
+  Outfile);
+}
+
+// Print out the assembly postamble
+void cgpostamble()
+{
+  fputs(
+	"\tmovl	$0, %eax\n"
+	"\tpopq	%rbp\n"
+	"\tret\n",
+  Outfile);
+}
+
+// Load an integer literal value into a register.
+// Return the number of the register
+int cgload(int value) {
+
+  // Get a new register
+  int r= alloc_register();
+
+  // Print out the code to initialise it
+  fprintf(Outfile, "\tmovq\t$%d, %s\n", value, reglist[r]);
+  return(r);
+}
+
+// Add two registers together and return
+// the number of the register with the result
+int cgadd(int r1, int r2) {
+  fprintf(Outfile, "\taddq\t%s, %s\n", reglist[r1], reglist[r2]);
+  free_register(r1);
+  return(r2);
+}
+
+// Multiply two registers together and return
+// the number of the register with the result
+int cgmul(int r1, int r2) {
+  fprintf(Outfile, "\timulq\t%s, %s\n", reglist[r1], reglist[r2]);
+  free_register(r1);
+  return(r2);
+}
+
+// Subtract the second register from the first and
+// return the number of the register with the result
+int cgsub(int r1, int r2) {
+  fprintf(Outfile, "\tsubq\t%s, %s\n", reglist[r2], reglist[r1]);
+  free_register(r2);
+  return(r1);
+}
+
+// Divide the first register by the second and
+// return the number of the register with the result
+int cgdiv(int r1, int r2) {
+  fprintf(Outfile, "\tmovq\t%s,%%rax\n", reglist[r1]);
+  fprintf(Outfile, "\tcqo\n");
+  fprintf(Outfile, "\tidivq\t%s\n", reglist[r2]);
+  fprintf(Outfile, "\tmovq\t%%rax,%s\n", reglist[r1]);
+  free_register(r2);
+  return(r1);
+}
+
+void cgprintint(int r) {
+  fprintf(Outfile, "\tmovq\t%s, %%rdi\n", reglist[r]);
+  fprintf(Outfile, "\tcall\tprintint\n");
+  free_register(r);
 }
