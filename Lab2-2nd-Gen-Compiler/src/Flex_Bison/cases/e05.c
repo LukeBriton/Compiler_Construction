4b0154b0b1fcb53b76f8cc2606d90927d15c4b7e
Montagnard <928786745@qq.com>
2024-05-07 13:08:35 +0800

    lab2 Flex_Bison Update cases

---
+++ Lab2-2nd-Gen-Compiler/src/Flex_Bison/cases/e05.c
@@ -0,0 +1,1 @@
+int main(){int _0893127890;int hgbahjkf;int or;int and;int xor;_0893127890=114;hgbahjkf=514;or=_0893127890|hgbahjkf;and=_0893127890&hgbahjkf;xor=_0893127890^hgbahjkf;println_int(or);println_int(and);println_int(xor);return 0;}
